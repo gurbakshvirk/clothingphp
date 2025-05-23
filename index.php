8c2ce67fe47eb963268cef6ddb8560fdbe0d017f
gurbakshvirk <lovyvirk34@gmail.com>
2025-05-23 18:48:33 +0530

    6.30pm 23/5/25

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include('includes/header.php'); ?>
+               <h1>Hello, world!</h1>
+               <button class="btn btn-primary">testing</button>
+
+
+<?php include('includes/footer.php');?>
+
